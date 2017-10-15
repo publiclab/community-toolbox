1f20a12cc703c6dd86737ce97ddce6f4b7620df9
jywarren <jeff@unterbahn.com>
2017-10-14 20:25:53 -0700

    basic contributor collection

--- src/chart.js
+++ src/chart.js
@@ -1,31 +1,51 @@
-
 // view-source:http://www.chartjs.org/samples/latest/charts/bar/vertical.html
+function generateChart(args) {
 
-var color = Chart.helpers.color;
+  args = args || {};
+  args.data = args.data || [];
+  args.label = args.label || "";
+  args.title = args.title || "";
+
+  var colors = {
+    "blue":   "rgb(54, 162, 235)",
+    "red":    "rgb(255, 99, 132)",
+    "green":  "rgb(75, 192, 192)",
+    "grey":   "rgb(201, 203, 207)",
+    "orange": "rgb(255, 159, 64)",
+    "purple": "rgb(153, 102, 255)",
+    "yellow": "rgb(255, 205, 86)"
+  }
+  var colorNames = Object.keys(colors);
+
   var barChartData = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    // labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [{
-      label: 'Dataset 1',
-      backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
-      borderColor: window.chartColors.red,
+      label: args.label,
+      backgroundColor: colors['red'],
+      borderColor: colors['red'],
       borderWidth: 1,
-      data: [
-      ]
+      data: args.data
     }]
+  }
+
+  var ctx = document.getElementById("canvas").getContext("2d");
+  var chart = new Chart(ctx, {
+    type: 'bar',
+    data: barChartData,
+    options: {
+      responsive: true,
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: args.title
+      }
+    }
+  });
+
+  return chart;
+
 }
 
-var ctx = document.getElementById("canvas").getContext("2d");
-window.myBar = new Chart(ctx, {
-  type: 'bar',
-  data: barChartData,
-  options: {
-    responsive: true,
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart'
-    }
-  }
-});
+module.exports = generateChart;
