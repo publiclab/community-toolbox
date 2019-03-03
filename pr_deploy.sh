209d905a086ed85022e6993fdfdf11cb627a8b4f
subhahu123 <jainsubhahu.gmail.com>
2019-03-03 14:41:49 +0530

    Adding dummy account

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -6,9 +6,9 @@ fi
 
 npm i -g surge
 
-export SURGE_LOGIN=jainsubhahu@gmail.com
+export SURGE_LOGIN=test@publiclab.org
 # Token of a dummy account.
-export SURGE_TOKEN=817c436dd0e89047b2f773b8c1b52d2b
+export SURGE_TOKEN=7abc79012b2bb1ad25e9dbba9349a231
 
 export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-community-toolbox.surge.sh
 surge --project ./ --domain $DEPLOY_DOMAIN;
