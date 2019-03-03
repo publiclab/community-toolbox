1ab0ac80ae802c8a29a50851adcff8098b761dcc
subhahu123 <jainsubhahu.gmail.com>
2019-03-03 14:10:06 +0530

    Adding surge link

--- pr_deploy.sh
+++ pr_deploy.sh
@@ -6,9 +6,9 @@ fi
 
 npm i -g surge
 
-export SURGE_LOGIN=test@public.co.in
+export SURGE_LOGIN=test@publiclab.co.in
 # Token of a dummy account.
-export SURGE_TOKEN=d1c28a7a75967cc2b4c852cca0d12206
+export SURGE_TOKEN=817c436dd0e89047b2f773b8c1b52d2b
 
 export DEPLOY_DOMAIN=https://pr-${TRAVIS_PULL_REQUEST}-community-toolbox.surge.sh
 surge --project ./ --domain $DEPLOY_DOMAIN;
