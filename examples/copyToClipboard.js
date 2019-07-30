9d2cb85e1b2e2fdf29a5b390959dbe1409033fd6
Kelvin Lien <Mr.Secret0987@gmail.com>
2019-07-30 07:56:40 +0700

    Update index.html, copyToClipboard.js and demo.css as Issue #240. (#242)

--- examples/copyToClipboard.js
+++ examples/copyToClipboard.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let usernames = document.getElementById('contrib-names');
-    let copyButton = document.getElementById('copybutton');
-    let dummyInp = document.createElement("input");
+    let copyButton = $(".copybutton");
     let body = document.querySelector('body');
-
-
-    copyButton.addEventListener("click", function (event) {
-        event.preventDefault();
-        let names = usernames.textContent;
-        dummyInp.setAttribute('type', 'text');
-		dummyInp.setAttribute('value', `${names}`);
-        body.appendChild(dummyInp);
-        dummyInp.select();
-		document.execCommand("copy");
-		dummyInp.setAttribute('type', 'hidden');
+    let dummyInp = document.createElement("input");
+    let usernames;
+    copyButton.map(function(item, ele) {
+        ele.addEventListener("click", function(event) {
+            event.preventDefault();
+            usernames = $(this).parent().children(this.lastChild).find(".usernames")[0];
+            let names = usernames.textContent;
+            dummyInp.setAttribute('type', 'text');
+            dummyInp.setAttribute('value', `${names}`);
+            body.appendChild(dummyInp);
+            dummyInp.select();
+            document.execCommand("copy");
+            dummyInp.setAttribute('type', 'hidden');
+        })
     })
 
 
