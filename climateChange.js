d38d1c0edffec7e9914ec9aa363c4a96666e213b
VirajGalappaththi <44154949+VirajGalappaththi@users.noreply.github.com>
2019-10-29 15:59:58 +0530

    Create climateChange.js

---
+++ climateChange.js
@@ -0,0 +1,12 @@
+alert("This page has finished loading.")
+confirm("Hey guys. It's nice to meet you")
+
+var global = prompt("Are you ready to learn?") 
+  if(global === "yes") 
+{
+    alert("Great! Let's get started!")
+}
+else 
+{
+    alert("Then come back when you are ready!")
+}
