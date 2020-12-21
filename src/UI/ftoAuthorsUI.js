8a108e01e966bdbd696edfa0d55d155a066f795f
Govind Goel <52847415+govindgoel@users.noreply.github.com>
2020-12-21 22:31:46 +0530

    add fto authors usernames (#416)
    This commit adds the fto authors username before the thumbnail
    Closes #415

--- src/UI/ftoAuthorsUI.js
+++ src/UI/ftoAuthorsUI.js
@@ -1,9 +1,12 @@
 function insertFtoIssueAuthor(issueSet) {
+    let usernames=[];
     let avatars=[];
     for(let [key, value] of Object.entries(issueSet)) {
+      usernames.push(`<a href="https://github.com/${value.user.login}">@${value.user.login}</a>`);
       avatars.push(`<a href="https://github.com/${value.user.login}" title="${value.user.login}"><img width="100px" src="${value.user.avatar_url}"></a>`);
     }
-    $('.fto-authors').append(avatars.join(' '));
+    $('.fto-authors > .usernames').html(usernames.join(', '));
+    $('.fto-authors > .avatars').html(avatars.join(' '));
   }
 
 
